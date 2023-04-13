d3a19d85ddee6cbd22b6d81b80fbf1c8d86574d9
haleyc <1617335718@qq.com>
2023-04-13 16:59:49 +0800

    增加注释

--- ALU/ALUTop.v
+++ ALU/ALUTop.v
@@ -2,63 +2,66 @@
 `include "ALU.v"
 `include "LED.v"
 
+// ALUTop模块，连接D_register、ALU和LED模块，实现多功能ALU的顶层模块
 module ALUTop(
-    input clk,
-	input clk_F,
-	input clk_A,
-	input clk_B,
-	input [31:0] SW,
-   output reg [3:0] F,
-   output [3:0] AN,
-   output [7:0] seg
+    input clk,        // 通用时钟信号
+    input clk_F,      // 用于控制数据寄存器和标志位寄存器的时钟信号
+    input clk_A,      // 控制寄存器A的时钟信号
+    input clk_B,      // 控制寄存器B的时钟信号
+    input [31:0] SW,  // 32位开关信号，用于提供操作数和操作码
+    output reg [3:0] F,   // 输出标志位，包括ZF、CF、OF和SF
+    output [3:0] AN,      // 输出信号，用于控制LED的阵列
+    output [7:0] seg      // 输出信号，用于控制LED的段选
 );
-	// D_register
-	wire [31:0] A;
-	wire [31:0] B;
-	reg [31:0] Data;
-    wire [31:0] Data_reg;
-    wire [3:0] Fr;
-	
-	// D_register
+    // D_register
+    wire [31:0] A;     // 寄存器A的输出
+    wire [31:0] B;     // 寄存器B的输出
+    reg [31:0] Data;   // 数据寄存器，用于存储ALU的计算结果
+    wire [31:0] Data_reg;  // ALU计算结果的临时存储
+    wire [3:0] Fr;     // ALU输出的标志位
+
+    // 实例化D_register模块，用于存储操作数A
     D_register u_D_registerA (
         .clk(clk_A),
         .in(SW[31:0]),
         .out(A)
     );
 
+    // 实例化D_register模块，用于存储操作数B
     D_register u_D_registerB (
         .clk(clk_B),
         .in(SW[31:0]),
         .out(B)
     );
-	
-	// ALU
+    
+    // 实例化ALU模块，根据操作码执行相应的操作
     ALU u_ALU (
         .a(A),
         .b(B),
         .op(SW[3:0]),
         .out(Data_reg),
-		  .ZF(Fr[3]),
-		  .CF(Fr[2]),
-		  .OF(Fr[1]),
-		  .SF(Fr[0])
+        .ZF(Fr[3]),
+        .CF(Fr[2]),
+        .OF(Fr[1]),
+        .SF(Fr[0])
     );
 
-    // LED
+    // 实例化LED模块，用于显示ALU的计算结果和标志位
     LED u_LED(
-		  .clk(clk),
-		  .Data(Data),
-		  .AN(AN),
-		  .seg(seg)
-	 );
+        .clk(clk),
+        .Data(Data),
+        .AN(AN),
+        .seg(seg)
+    );
 
-     always @(posedge clk_F) begin
+    // 在clk_F上升沿时，将ALU的计算结果存储到数据寄存器中
+    always @(posedge clk_F) begin
         Data <= Data_reg;
-     end
+    end
 
-     always @(posedge clk_F) begin
-            F <= Fr;
-     end
+    // 在clk_F上升沿时，将ALU输出的标志位存储到标志位寄存器中
+    always @(posedge clk_F) begin
+        F <= Fr;
+    end
 
-	 
 endmodule
--- ALU/LED.v
+++ ALU/LED.v
@@ -1,32 +1,29 @@
 module LED(
-    input clk,
-	 input[31:0] Data,
-    output[3:0] AN,
-    output[7:0] seg 
+    input clk,               // 输入时钟信号
+    input [31:0] Data,       // 32位输入数据
+    output [3:0] AN,         // 数码管选通信号输出
+    output [7:0] seg         // 数码管段选信号输出
 );
 
-    wire clk_500HZ;
-    C500HZ user2(clk,clk_500HZ);
-    show u4(clk_500HZ,Data,AN,seg);
-	 
+    wire clk_500HZ;          // 定义一个500Hz的时钟信号
+    C500HZ user2(clk, clk_500HZ); // 时钟分频模块，用于生成500Hz的时钟信号
+    show u4(clk_500HZ, Data, AN, seg); // 数码管显示模块
+
 endmodule
 
-
-
-
-
-module C500HZ( // 每个数码管选通时间为2ms,即转换信号为500HZ 
-  input clk_old,
-  output reg clk_new
+// 时钟分频模块，用于生成每个数码管选通时间为2ms，即转换信号为500Hz的时钟信号
+module C500HZ(
+  input clk_old,            // 输入原始时钟信号
+  output reg clk_new        // 输出分频后的时钟信号
 );
-  reg[15:0] counter;
+  reg [15:0] counter;       // 定义一个16位计数器
   initial begin
     counter <= 16'd0;
-	 clk_new<=1'b0;
+    clk_new <= 1'b0;
   end
 
   always @(posedge clk_old) begin
-      if (counter>=16'd25_000) begin
+      if (counter >= 16'd25_000) begin
         clk_new <= ~clk_new;
         counter <= 16'b0;
       end
@@ -35,89 +32,83 @@ module C500HZ( // 每个数码管选通时间为2ms,即转换信号为500HZ
     end
 endmodule
 
-
-
-
-
+// 数码管显示模块
 module show(
-    	input clk,
-    	input [31:0]Data,
-    	output reg [3:0]AN,
-    	output reg [7:0]seg
-        );
-    	
-		reg [2:0]Bit_Sel;
-		reg [3:0]data;
-		initial begin 
-		Bit_Sel <= 3'd0;
-		data <= 4'd0;
-		AN <= 4'b1111;
-		end
-		always@(posedge clk)
-			begin
-				Bit_Sel<=Bit_Sel+1'b1;
-				
-				case(Bit_Sel)
-					3'b000:begin
-							AN<=4'b1111;
-							data<=Data[3:0];
-							end
-					3'b001:begin
-							AN<=4'b1110;
-							data<=Data[7:4];
-							end
-					3'b010:begin
-							AN<=4'b1101;
-							data<=Data[11:8];
-							end
-					3'b011:
-							begin
-							AN<=4'b1100;
-							data<=Data[15:12];
-							end
-					3'b100:
-							begin
-							AN<=4'b1011;
-							data<=Data[19:16];
-							end
-					3'b101:
-							begin
-							AN<=4'b1010;
-							data<=Data[23:20];
-							end
-					3'b110:
-							begin
-							AN<=4'b1001;
-							data<=Data[27:24];
-							end
-					3'b111:
-							begin
-							AN<=4'b1000;
-							data<=Data[31:28];
-							end
-					//default:AN<=2'b1111;
-				endcase
+    input clk,               // 输入时钟信号，这里为500Hz
+    input [31:0] Data,       // 32位输入数据
+    output reg [3:0] AN,     // 数码管选通信号输出
+    output reg [7:0] seg     // 数码管段选信号输出
+);
+
+    reg [2:0] Bit_Sel;       // 3位位选计数器
+    reg [3:0] data;          // 当前显示的4位数码管数据
+    initial begin 
+        Bit_Sel <= 3'd0;
+        data <= 4'd0;
+        AN <= 4'b1111;
+    end
+	
+    always @(posedge clk) begin
+        Bit_Sel <= Bit_Sel + 1'b1;
+
+        // 根据位选计数器的值选择数码管显示的数据
+        case (Bit_Sel)
+            3'b000: begin
+                AN <= 4'b1111;
+                data <= Data[3:0];
+            end
+            3'b001: begin
+                AN <= 4'b1110;
+                data <= Data[7:4];
+            end
+            3'b010: begin
+                AN <= 4'b1101;
+                data <= Data[11:8];
+            end
+            3'b011: begin
+                AN <= 4'b1100;
+                data <= Data[15:12];
+            end
+            3'b100: begin
+                AN <= 4'b1011;
+                data <= Data[19:16];
+            end
+            3'b101: begin
+                AN <= 4'b1010;
+                data <= Data[23:0];
 			end
-		
-		always@(*)
-			begin
-			case(data)
-    			4'b0000: seg<=8'b00000011;//0
-    			4'b0001: seg<=8'b10011111;//1
-    			4'b0010: seg<=8'b00100101;//2
-    			4'b0011: seg<=8'b00001101;//3
-    			4'b0100: seg<=8'b10011001;//4
-    			4'b0101: seg<=8'b01001001;//5
-    			4'b0110: seg<=8'b01000001;//6
-    			4'b0111: seg<=8'b00011111;//7
-    			4'b1000: seg<=8'b00000001;//8
-    			4'b1001: seg<=8'b00001001;//9
-    			4'b1010: seg<=8'b00010001;//a
-    			4'b1011: seg<=8'b11000001;//b
-    			4'b1100: seg<=8'b01100011;//c
-    			4'b1101: seg<=8'b10000101;//d
-    			4'b1110: seg<=8'b01100001;//e
-    			4'b1111: seg<=8'b01110001;//f
-    		endcase
+			3'b110: begin
+				AN <= 4'b1001;
+				data <= Data[27:24];
 			end
+			3'b111: begin
+				AN <= 4'b1000;
+				data <= Data[31:28];
+			end
+		//default:AN<=2'b1111;
+		endcase
+	end
+
+	always@(*) begin
+		// 根据当前显示的数据，输出对应的数码管段选信号
+		case (data)
+			4'b0000: seg <= 8'b00000011; // 0
+			4'b0001: seg <= 8'b10011111; // 1
+			4'b0010: seg <= 8'b00100101; // 2
+			4'b0011: seg <= 8'b00001101; // 3
+			4'b0100: seg <= 8'b10011001; // 4
+			4'b0101: seg <= 8'b01001001; // 5
+			4'b0110: seg <= 8'b01000001; // 6
+			4'b0111: seg <= 8'b00011111; // 7
+			4'b1000: seg <= 8'b00000001; // 8
+			4'b1001: seg <= 8'b00001001; // 9
+			4'b1010: seg <= 8'b00010001; // A
+			4'b1011: seg <= 8'b11000001; // B
+			4'b1100: seg <= 8'b01100011; // C
+			4'b1101: seg <= 8'b10000101; // D
+			4'b1110: seg <= 8'b01100001; // E
+			4'b1111: seg <= 8'b01110001; // F
+		endcase
+	end
+
 endmodule
