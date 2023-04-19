de2ce1f3f34085f53f503c2daded55a3c877e740
haleyc <1617335718@qq.com>
2023-04-19 12:50:30 +0800

    编写测试用例

--- RegArray/RegATop_tb.v
+++ RegArray/RegATop_tb.v
@@ -1,5 +1,7 @@
 //~ `New testbench
 `timescale  1ns / 1ps  
+`include "RegATop.v"
+
 
 module tb_RegATop;     
 
@@ -46,10 +48,117 @@ RegATop  u_RegATop (
     .FR                      ( FR         [3:0] )
 );
 
+integer i;
+
 initial
 begin
     $dumpfile("RegATop.vcd");
     $dumpvars(0, tb_RegATop);
+
+    #100;
+    R_Addr_A = 0;
+    R_Addr_B = 0;
+    W_Addr = 0;
+    ALU_OP = 3;
+    Reg_Write = 0;
+    clk_RR = 0;
+
+    // init
+    // 全部置0
+    for (i = 0; i < 32; i = i + 1) begin
+        #100;
+        Reg_Write = 0;
+        R_Addr_A = i;
+        R_Addr_B = i;
+        W_Addr = i;
+        ALU_OP = 3;
+        #10;
+        clk_RR = 1;
+        #10;
+        clk_RR = 0;
+
+        #10;
+        clk_F = 1;
+        #10;
+        clk_F = 0;
+
+        Reg_Write = 1;
+        #10;
+        clk_WB = 1;
+        #10;
+        Reg_Write = 0;
+        clk_WB = 0;
+    end
+
+
+    // 第i位置i
+    for (i = 1; i < 32; i = i + 1) begin
+        #100;
+        Reg_Write = i;
+        R_Addr_A = i-1;
+        R_Addr_B = i-1;
+        W_Addr = i;
+        ALU_OP = 10;
+        #10;
+        clk_RR = 1;
+        #10;
+        clk_RR = 0;
+
+        // 输出
+        #10;
+        clk_F = 1;
+        #10;
+        clk_F = 0;
+
+        // 写回
+        Reg_Write = 1;
+        #10;
+        clk_WB = 1;
+        #10;
+        Reg_Write = 0;
+        clk_WB = 0;
+    end
+
+    // 遍历
+    for (i = 0; i < 32; i = i + 1) begin
+        #100;
+        Reg_Write = i;
+        R_Addr_A = 0;
+        R_Addr_B = i;
+        W_Addr = 0;
+        ALU_OP = 0;
+        #10;
+        clk_RR = 1;
+        #10;
+        clk_RR = 0;
+
+        // 输出
+        #10;
+        clk_F = 1;
+        #10;
+        clk_F = 0;
+    end
+
+    // 输出第i位左移i位
+    for (i = 1; i < 32; i = i + 1) begin
+        #100;
+        Reg_Write = 0;
+        R_Addr_A = 1;
+        R_Addr_B = i;
+        W_Addr = i;
+        ALU_OP = 1;
+        #10;
+        clk_RR = 1;
+        #10;
+        clk_RR = 0;
+
+        // 输出
+        #10;
+        clk_F = 1;
+        #10;
+        clk_F = 0;
+
+    end
 
 
     $finish;
