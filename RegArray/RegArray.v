9eb7b59a6e98528bbe89fe6833881b3cbecf4ec7
haleyc <1617335718@qq.com>
2023-04-19 11:14:20 +0800

    移除5_32解码器

--- RegArray/RegArray.v
+++ RegArray/RegArray.v
@@ -1,4 +1,3 @@
-`include "decoder_5_32.v"
 
 module RegArray (
     input clk_Regs,
@@ -11,10 +10,9 @@ module RegArray (
     output [31:0] R_Data_B
 );
     reg [31:0] REG_Files[0:31];
-    wire [31:0] W_Addr_32,R_Addr_A_32,R_Addr_B_32;
     
-    assign R_Data_A=REG_Files[R_Addr_A_32];
-    assign R_Data_B=REG_Files[R_Addr_B_32];
+    assign R_Data_A=REG_Files[R_Addr_A];
+    assign R_Data_B=REG_Files[R_Addr_B];
 
     integer i;
 
@@ -25,22 +23,10 @@ module RegArray (
         end
     end
 
-    Decoder_5_32 uW(
-        .in(W_Addr),
-        .out(W_Addr_32)
-    );
-    Decoder_5_32 uA(
-        .in(R_Addr_A),
-        .out(R_Addr_A_32)
-    );
-    Decoder_5_32 uB(
-        .in(R_Addr_B),
-        .out(R_Addr_B_32)
-    );
 
     always@(posedge clk_Regs) begin
         if (Reg_Write) begin
-            REG_Files[W_Addr_32] <=W_Data;
+            REG_Files[W_Addr] <=W_Data;
         end
     end
 
--- RegArray/decoder_5_32.v
+++
@@ -1,8 +0,0 @@
-module Decoder_5_32(
-    input [4:0] in,
-    output [31:0] out
-);
-    reg [31:0] base = 32'h00000001;
-    assign out = base<<in;
-
-endmodule
