0781e88dfd808483e35dd4ccfb5368166263216d
haleyc <1617335718@qq.com>
2023-04-20 14:29:37 +0800

    新增注释

--- RegArray/RegATop.v
+++ RegArray/RegATop.v
@@ -1,103 +1,95 @@
-`include "./ALU/ALU.v" 
-`include "./ALU/LED.v" 
-`include "./ALU/Reg.v" 
-`include "RegArray.v" 
+include "./ALU/ALU.v" include "./ALU/LED.v"
+include "./ALU/Reg.v" include "RegArray.v"
+
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    15:03:52 04/13/2023 
-// Design Name: 
-// Module Name:    RegATop 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Company:
+// Engineer:
 //
-// Dependencies: 
+// Create Date: 15:03:52 04/13/2023
+// Design Name:
+// Module Name: RegATop
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Revision: 
+// Dependencies:
+//
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
 module RegATop(
-	 input clk,
-	 input[4:0] R_Addr_A,
-	 input[4:0] R_Addr_B,
-	 input[4:0] W_Addr,
-	 input[3:0] ALU_OP,
-	 input Reg_Write,
-	 input clk_RR,
-	 input clk_F,
-	 input clk_WB,
-	 output [3:0] AN,
-    output [7:0] seg,
-	 output reg [3:0] FR
-    );
-	reg [31:0] F;
-	wire[31:0] R_Data_A;
-	wire[31:0] R_Data_B;
-	// reg [31:0] Data;
-    wire [31:0] Data_reg;
-    wire [3:0] Fr;
+	input clk, // 时钟信号输入
+	input[4:0] R_Addr_A, // 读寄存器地址A输入
+	input[4:0] R_Addr_B, // 读寄存器地址B输入
+	input[4:0] W_Addr, // 写寄存器地址输入
+	input[3:0] ALU_OP, // ALU操作信号输入
+	input Reg_Write, // 寄存器写使能信号输入
+	input clk_RR, // 寄存器读时钟信号输入
+	input clk_F, // ALU操作时钟信号输入
+	input clk_WB, // 寄存器写时钟信号输入
+	output [3:0] AN, // 数码管段选信号输出
+	output [7:0] seg, // 数码管段信号输出
+	output reg [3:0] FR // 标志寄存器输出
+);
+	reg [31:0] F;          // 输出数据寄存器
+	wire[31:0] R_Data_A;   // 读取数据A
+	wire[31:0] R_Data_B;   // 读取数据B
+	wire [31:0] Data_reg;  // ALU输出数据
+	wire [3:0] Fr;         // ALU标志寄存器
 
 	wire [31:0] R_Data_A_;
 	wire [31:0] R_Data_B_;
 
-	 // RegArray
-	 RegArray RegArr(
-			.clk_Regs(clk_WB),
-			.Reg_Write(Reg_Write),
-			.R_Addr_A(R_Addr_A),
-			.R_Addr_B(R_Addr_B),
-			.W_Addr(W_Addr),
-			.W_Data(F),
-			.R_Data_A(R_Data_A),
-			.R_Data_B(R_Data_B)
-	 );
-	 // RegA
-	 D_register u_D_registerA (
-        .clk(clk_RR),
-        .in(R_Data_A[31:0]),
-        .out(R_Data_A_)
-    );
-	 // RegB
-	 D_register u_D_registerB (
-        .clk(clk_RR),
-        .in(R_Data_B[31:0]),
-        .out(R_Data_B_)
-    );
-	 
-	 
-	 	// ALU
-    ALU u_ALU (
-        .a(R_Data_A_),
-        .b(R_Data_B_),
-        .op(ALU_OP),
-        .out(Data_reg),
-		  .ZF(Fr[3]),
-		  .CF(Fr[2]),
-		  .OF(Fr[1]),
-		  .SF(Fr[0])
-    );
+	// 寄存器阵列实例化
+	RegArray RegArr(
+		.clk_Regs(clk_WB),
+		.Reg_Write(Reg_Write),
+		.R_Addr_A(R_Addr_A),
+		.R_Addr_B(R_Addr_B),
+		.W_Addr(W_Addr),
+		.W_Data(F),
+		.R_Data_A(R_Data_A),
+		.R_Data_B(R_Data_B)
+	);
+	// 寄存器A实例化
+	D_register u_D_registerA (
+		.clk(clk_RR),
+		.in(R_Data_A[31:0]),
+		.out(R_Data_A_)
+	);
+	// 寄存器B实例化
+	D_register u_D_registerB (
+		.clk(clk_RR),
+		.in(R_Data_B[31:0]),
+		.out(R_Data_B_)
+	);
 
-    // LED
-    LED u_LED(
-		  .clk(clk),
-		  .Data(F),
-		  .AN(AN),
-		  .seg(seg)
-	 );
+	// ALU实例化
+	ALU u_ALU (
+		.a(R_Data_A_),
+		.b(R_Data_B_),
+		.op(ALU_OP),
+		.out(Data_reg),
+		.ZF(Fr[3]),
+		.CF(Fr[2]),
+		.OF(Fr[1]),
+		.SF(Fr[0])
+	);
 
-     always @(posedge clk_F) begin
-        F <= Data_reg;
-		  FR <= Fr;
-     end
+	// LED实例化
+	LED u_LED(
+		.clk(clk),
+	.Data(F),
+	.AN(AN),
+	.seg(seg)
+	);
+	// 时钟上升沿触发ALU输出数据和标志寄存器更新
+	always @(posedge clk_F) begin
+		F <= Data_reg;
+		FR <= Fr;
+	end
 
-    //  always @(posedge clk_F) begin
-    //         F <= Fr;
-    //  end
-	 
-	 
 endmodule
--- RegArray/RegArray.v
+++ RegArray/RegArray.v
@@ -1,33 +1,37 @@
-
 module RegArray (
-    input clk_Regs,
-    input Reg_Write,
-    input [4:0] R_Addr_A,
-    input [4:0] R_Addr_B,
-    input [4:0] W_Addr, 
-    input [31:0] W_Data,
-    output [31:0] R_Data_A,
-    output [31:0] R_Data_B
+    input clk_Regs, // 时钟信号输入
+    input Reg_Write, // 寄存器写使能信号
+    input [4:0] R_Addr_A, // 读寄存器地址A输入
+    input [4:0] R_Addr_B, // 读寄存器地址B输入
+    input [4:0] W_Addr, // 写寄存器地址输入
+    input [31:0] W_Data, // 写入数据输入
+    output [31:0] R_Data_A, // 读取数据A输出
+    output [31:0] R_Data_B // 读取数据B输出
 );
-    reg [31:0] REG_Files[0:31];
-    
-    assign R_Data_A=REG_Files[R_Addr_A];
-    assign R_Data_B=REG_Files[R_Addr_B];
+    reg [31:0] REG_Files[0:31]; // 定义32个寄存器
 
-    integer i;
+    // 将读取地址A对应的寄存器数据赋值给R_Data_A
+    assign R_Data_A = REG_Files[R_Addr_A];
+    // 将读取地址B对应的寄存器数据赋值给R_Data_B
+    assign R_Data_B = REG_Files[R_Addr_B];
 
+    integer i; // 定义循环变量
+
+    // 初始化寄存器数组
     initial begin
-        for (i=0;i<32;i=i+1) begin
-            REG_Files[i]=32'h00000000;
-            $display("REG_Files[%d]=%h",i,REG_Files[i]);
+        for (i = 0; i < 32; i = i + 1) begin
+            REG_Files[i] = 32'h00000000;
+            // 显示初始化寄存器值
+            $display("REG_Files[%d] = %h", i, REG_Files[i]);
         end
     end
 
-
+    // 时钟上升沿触发
     always@(posedge clk_Regs) begin
+        // 如果写使能信号为1，则将W_Data写入W_Addr指定的寄存器
         if (Reg_Write) begin
-            REG_Files[W_Addr] <=W_Data;
+            REG_Files[W_Addr] <= W_Data;
         end
     end
 
-endmodule
+endmoduleß
