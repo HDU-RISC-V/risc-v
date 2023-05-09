4ecc5a870c1baf5403ec8b2a2048574a5a8374d0
haleyc <1617335718@qq.com>
2023-05-09 14:38:14 +0800

    add comments

--- RAM/RAM.v
+++ RAM/RAM.v
@@ -1,34 +1,31 @@
 module RAM (
-    input Mem_Write,
-    input [5:0] DM_Addr,
-    input [31:0] M_W_Data,
-    input clk_dm,
-    output [31:0] M_R_Data
+    input Mem_Write,            // 写使能信号
+    input [5:0] DM_Addr,       // 读写地址
+    input [31:0] M_W_Data,     // 写入的数据
+    input clk_dm,              // 时钟信号
+    output [31:0] M_R_Data     // 读出的数据
 );
-    reg [31:0] M_R_Data_reg_00 [0:31];
-    reg [31:0] M_R_Data_reg_01 [0:31];
+    reg [31:0] M_R_Data_reg_00 [0:31]; // 存储器数据寄存器1
+    reg [31:0] M_R_Data_reg_01 [0:31]; // 存储器数据寄存器2
     integer i;
 
-    assign M_R_Data = DM_Addr[5]?M_R_Data_reg_01[DM_Addr[4:0]]:M_R_Data_reg_00[DM_Addr[4:0]];
+    assign M_R_Data = DM_Addr[5]?M_R_Data_reg_01[DM_Addr[4:0]]:M_R_Data_reg_00[DM_Addr[4:0]]; // 将M_R_Data与M_R_Data_reg中对应的数据相连
 
-
-    initial begin
+    initial begin                        // 初始化内存数据
         for (i = 0; i<32; ++i) begin
-            M_R_Data_reg_00[i] = 32'h00000000;
-            M_R_Data_reg_01[i] = 32'h00000000;
+            M_R_Data_reg_00[i] = 32'h00000000; // 初始化存储器数据寄存器1
+            M_R_Data_reg_01[i] = 32'h00000000; // 初始化存储器数据寄存器2
         end
     end
 
-    always@(posedge clk_dm) begin
-        if (DM_Addr[5]==0) begin
-            M_R_Data_reg_00[DM_Addr[4:0]] <= M_W_Data;
-            $display("M_R_Data_reg_00[%d] <= %d", DM_Addr[4:0], M_W_Data);
+    always@(posedge clk_dm) begin          // 在时钟信号上升沿时执行
+        if (DM_Addr[5]==0) begin           // 判断写入哪一个存储器
+            M_R_Data_reg_00[DM_Addr[4:0]] <= M_W_Data; // 写入存储器数据寄存器1
+            $display("M_R_Data_reg_00[%d] <= %d", DM_Addr[4:0], M_W_Data); // 输出写入的地址和数据
         end else begin
-            M_R_Data_reg_01[DM_Addr[4:0]] <= M_W_Data;
-            $display("M_R_Data_reg_01[%d] <= %d", DM_Addr[4:0], M_W_Data);
+            M_R_Data_reg_01[DM_Addr[4:0]] <= M_W_Data; // 写入存储器数据寄存器2
+            $display("M_R_Data_reg_01[%d] <= %d", DM_Addr[4:0], M_W_Data); // 输出写入的地址和数据
         end
-        $display("M_R_Data = %d", M_R_Data);
+        $display("M_R_Data = %d", M_R_Data); // 输出读出的数据
     end
-
-
 endmodule
--- RAM/RAMTop.v
+++ RAM/RAMTop.v
@@ -21,64 +21,51 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module RAMTop(
-   input clk,
-	input Mem_Write,
-	input[7:2] DM_Addr,
-	input[1:0] MW_Data_s,
-	input clk_dm,
-	output[3:0] AN,
-	output[7:0] seg
+   input clk,                 // 时钟信号
+    input Mem_Write,           // 写使能信号
+    input[7:2] DM_Addr,        // 读写地址
+    input[1:0] MW_Data_s,      // 写入的数据选择信号
+    input clk_dm,              // 存储器时钟信号
+    output[3:0] AN,            // 数码管位选信号
+    output[7:0] seg            // 数码管数值输出信号
     );
-	 wire [31:0] M_R_Data;
-	 reg[31:0] M_W_Data;
-	 reg[31:0] A = 32'b00000000_00000000_00000000_00000010;
-	 reg[31:0] B = 32'b00000000_00000000_00000000_00000011;
-	 reg[31:0] C = 32'b00000000_00000000_00000000_00000110;
-	 reg[31:0] D = 32'b00000000_00000000_00000000_00000111;
-	 initial begin 
-		 A = 32'b00000000_00000000_00000000_00000010;
-		 B = 32'b00000000_00000000_00000000_00000011;
-		 C = 32'b00000000_00000000_00000000_00000110;
-		 D = 32'b00000000_00000000_00000000_00000111;
-	 end
-	 
-	 always@(*)begin
-	 //if(MW_Data_s[1:0] == 2'b00)begin
-	 //M_W_Data <= A;
-	 //end
-	 //else if(MW_Data_s[1:0] == 2'b01)begin
-	 //M_W_Data <= B;
-	 //end
-	 //else if(MW_Data_s[1:0] == 2'b10)begin
-	 //M_W_Data <= C;
-	 //end
-	 //else if(MW_Data_s[1:0] == 2'b11)begin
-	 //M_W_Data <= D;
-	 //end
-	 
-	 case(MW_Data_s)
-	 2'b00:begin M_W_Data <= A; end
-	 2'b01:begin M_W_Data <=B; end
-	 2'b10:begin M_W_Data <=C; end
-	 2'b11:begin M_W_Data <=D; end
-	 endcase
-	 //M_W_Data <= MW_Data_s;
-	 end
-	 
-	 RAM RAM(
-	 .Mem_Write(Mem_Write),
-	 .DM_Addr(DM_Addr),
-	 .M_W_Data(M_W_Data),
-	 .clk_dm(clk_dm),
-	 .M_R_Data(M_R_Data)
-	 );
-	 
-	 LED led(
-	 .clk(clk),
-	 .Data(M_R_Data),
-	 .AN(AN),
-	 .seg(seg)
-	 );
-	 
-	 
+     wire [31:0] M_R_Data;       // 存储器读出数据
+     reg[31:0] M_W_Data;         // 存储器写入数据
+     reg[31:0] A = 32'b00000000_00000000_00000000_00000010; // 待写入的数据A
+     reg[31:0] B = 32'b00000000_00000000_00000000_00000011; // 待写入的数据B
+     reg[31:0] C = 32'b00000000_00000000_00000000_00000110; // 待写入的数据C
+     reg[31:0] D = 32'b00000000_00000000_00000000_00000111; // 待写入的数据D
+     
+     initial begin 
+         A = 32'b00000000_00000000_00000000_00000010; // 初始化待写入的数据A
+         B = 32'b00000000_00000000_00000000_00000011; // 初始化待写入的数据B
+         C = 32'b00000000_00000000_00000000_00000110; // 初始化待写入的数据C
+         D = 32'b00000000_00000000_00000000_00000111; // 初始化待写入的数据D
+     end
+     
+     always@(*)begin
+     case(MW_Data_s)  // 根据MW_Data_s的值选择要写入的数据
+		2'b00:begin M_W_Data <= A; end
+		2'b01:begin M_W_Data <=B; end
+		2'b10:begin M_W_Data <=C; end
+		2'b11:begin M_W_Data <=D; end
+     endcase
+     end
+     
+     RAM RAM(                   // 实例化RAM模块
+     .Mem_Write(Mem_Write),
+     .DM_Addr(DM_Addr),
+     .M_W_Data(M_W_Data),
+     .clk_dm(clk_dm),
+     .M_R_Data(M_R_Data)
+     );
+     
+     LED led(                    // 实例化LED模块
+     .clk(clk),
+     .Data(M_R_Data),
+     .AN(AN),
+     .seg(seg)
+     );
+     
 endmodule
+
