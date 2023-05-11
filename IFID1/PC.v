4370f8f7e6679a41aaa27ab38760ee5916fba483
haleyc <1617335718@qq.com>
2023-05-11 14:16:49 +0800

    finish PC.v

---
+++ IFID1/PC.v
@@ -0,0 +1,17 @@
+module PC(
+    input [31:0] pc_in,
+    input PC_Write,
+    input clk,
+    output reg [31:0] pc_out,
+)
+initial begin
+    pc_out = 0;
+end
+
+always @(posedge clk) begin
+    if (PC_Write) begin
+        pc_out = pc_in;
+    end
+end
+
+endmodule
