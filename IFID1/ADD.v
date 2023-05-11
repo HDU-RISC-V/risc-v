781a7e1a92387f5f4762d3de0cd119c78f85db6f
haleyc <1617335718@qq.com>
2023-05-11 14:25:37 +0800

    bug fix

--- IFID1/ADD.v
+++ IFID1/ADD.v
@@ -1,7 +1,7 @@
 module ADD(
     input [31:0] a,
     input [31:0] b,
-    output reg [31:0] out,
+    output [31:0] out
 );
     assign out = a + b;
 endmodule
--- IFID1/PC.v
+++ IFID1/PC.v
@@ -2,8 +2,8 @@ module PC(
     input [31:0] pc_in,
     input PC_Write,
     input clk,
-    output reg [31:0] pc_out,
-)
+    output reg [31:0] pc_out
+);
 initial begin
     pc_out = 0;
 end
