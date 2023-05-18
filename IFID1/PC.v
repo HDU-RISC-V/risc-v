729372e5802f5fb5e00e5ed999266794d5bd5ad6
haleyc <1617335718@qq.com>
2023-05-18 14:22:36 +0800

    add rst_n

--- IFID1/PC.v
+++ IFID1/PC.v
@@ -2,15 +2,19 @@ module PC(
     input [31:0] PC_In,
     input PC_Write,
     input clk,
+    input rst_n,
     output reg [31:0] PC_Out
 );
 initial begin
     PC_Out = 0;
 end
 
-always @(posedge clk) begin
-    if (PC_Write) begin
-        PC_Out = PC_In;
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+        PC_Out <= 0;
+    end
+    else if(PC_Write) begin
+        PC_Out <= PC_In;
     end
 end
 
